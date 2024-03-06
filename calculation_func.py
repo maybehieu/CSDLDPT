9bc06726d30f919802bb47338ea1308972469664
maybehieu <71547583+maybehieu@users.noreply.github.com>
2024-03-07 00:00:24 +0700

    minor update

--- calculation_func.py
+++ calculation_func.py
@@ -19,3 +19,21 @@ def calculate_average_power(y, sr):
     # mean
     average_power = np.mean(power)
     return average_power
+
+
+def calculate_average_frequency(y, sr):
+    # Calculate Short-Time Fourier Transform (STFT)
+    D = librosa.stft(y)
+    # Compute the magnitude spectrum
+    magnitude_spectrum = np.abs(D)
+    # Calculate the frequency vector
+    frequency_vector = librosa.fft_frequencies(sr=sr)
+    # Calculate the weighted sum of frequencies
+    weighted_sum = np.sum(magnitude_spectrum * frequency_vector)
+    # Calculate the total energy
+    total_energy = np.sum(magnitude_spectrum)
+    # Calculate the average frequency
+    average_frequency = weighted_sum / total_energy
+    return average_frequency
+
+
